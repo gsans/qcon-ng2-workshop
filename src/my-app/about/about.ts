200d9af36118d6d1dc64c4e878f4d64f4415a74d
Gerard Sans <gerard.sans@bigsystemstechnology.com>
2016-03-11 03:15:15 +0000

    Unit test example.

--- src/my-app/about/about.ts
+++ src/my-app/about/about.ts
@@ -1,11 +1,21 @@
 import {Component} from 'angular2/core';
+import {LanguagesService} from '../services/languagesService';
 
 @Component({
   selector: 'about',
-  template: `<h1>About</h1>`
+  providers: [
+    LanguagesService
+  ],
+  template: `
+    <h1>About</h1>
+    Languages: <span *ngFor="#language of languages">{{language}} </span>
+  `
 })
 export class About {
-  constructor() { }
+  languages;
+  constructor(private service:LanguagesService) {
+    this.languages = service.get();
+  }
 
   ngOnInit() {
     console.log('hello `About` component');
